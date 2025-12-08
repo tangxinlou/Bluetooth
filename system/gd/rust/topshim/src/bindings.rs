2a72cd8544e9bfc01b2864a4c6d1d3fa77ab1c66
tangxinlou <tangxinlou@txl.com>
2025-12-08 10:19:03 +0800

    master 不保存代码，空出来写文档

--- system/gd/rust/topshim/src/bindings.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-// TODO(b/203002625) - since rustc 1.53, bindgen causes UB warnings
-// Remove this once bindgen figures out how to do this correctly
-#![allow(deref_nullptr)]
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
--- system/gd/rust/topshim/src/profiles/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Various libraries to access the profile interfaces.
-
-pub mod a2dp;
-pub mod avrcp;
-pub mod gatt;
-pub mod hfp;
-pub mod hid_host;
-pub mod sdp;
